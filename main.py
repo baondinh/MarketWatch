d2fdc58ef878cc2f13f2d5b42d062aa4b178da00
baondinh <bao.n.dinh@gmail.com>
2025-08-12 07:01:33 -0700

    working marketwatch api calls and CSV output

--- main.py
+++ main.py
@@ -2,10 +2,45 @@ import logging
 import smtplib
 import os
 from dotenv import load_dotenv
+import requests
+import pandas as pd
 
-def get_marketwatch(api_key: str): 
-    print('test')
 
+'''
+There are several API endpoints to choose from:
+
+    End-of-Day Data: Get daily stock market data.
+    Intraday Data: Get intraday and real-time market data.
+    Tickers: Get information about stock ticker symbols.
+    Exchanges: Get infotmation about all supported exchanges.
+    Currencies: Get information about all supported currencies.
+    Timezones: Get information about all supported timezones.
+
+Base URL: API requests start out with the following base URL:
+
+http://api.marketstack.com/v1/
+'''
+
+# dates YYYY-MM-DD
+# EOD or intraday endpoints both return historic if providing date_from and date_to params
+def get_marketwatch_historic(symbols, date_from, date_to, api_key: str): 
+    url = 'http://api.marketstack.com/v1/eod'
+
+    params = {
+        "access_key": api_key,
+        "symbols": ",".join(symbols),  # supports multiple tickers
+        "date_from": date_from,
+        "date_to": date_to
+    }
+    
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return data.get("data", [])
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return []
 
 def main(): 
     # Load environment variables from .env file
@@ -32,7 +67,19 @@ def main():
     except Exception as e: 
         logger.error(f"Failed to setup Outlook email {e}")
 
-    
+    logger.info("Fetching marketwatch data")
+    date_from = '2025-08-04'
+    date_to = '2025-08-08'
+    symbols = ['AAPL']
+    results = get_marketwatch_historic(symbols, date_from, date_to, MARKET_API_KEY)
+
+    if results:
+        df = pd.DataFrame(results)
+        output_file = f'market_data_{date_from}_{date_to}.csv'
+        df.to_csv(f"output/{output_file}", index=False)
+        logger.info(f"Data successfully saved as {output_file}")
+    else:
+        logger.error("No data to save.")
 
 if __name__ == "__main__": 
     main()
